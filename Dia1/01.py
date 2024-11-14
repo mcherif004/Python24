46f26a0607c6a606a15050b3794d6fa9b763270f
mcherif004 <a23chmomo@iesgrancapitan.org>
2024-11-14 23:26:26 +0100

    Ejercicio 1 (a-e)

--- Dia1/01.py
+++ Dia1/01.py
@@ -9,3 +9,42 @@ c) Total de los 100 elementos de punto-flotante de un array c.
 d) Copia los 11 elementos de un array a en la primera porción de un array b, el cual contiene 34 elementos. 
 e) Calcula y muestra el valor mayor y menor contenidos en un array w de 99 elementos de punto-flotante
 """
+
+# Apartado a)
+
+f = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(f[5])
+
+# Apartado b)
+
+array = [0] * 10 # Sera un array con 10 ceros
+
+for i in range(5):
+    array[i] = 8
+
+print(array)
+
+# Apartado c)
+
+c = [0.1] * 100
+
+total = sum(c)
+
+print(total)
+
+# Apartado d)
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] 
+b = [0] * 34
+
+b[:11] = a # Con los puntos delante coloca al principio y al final los pone al final
+
+print(b)
+
+# Apartado e)
+
+w = [1.5, 1.2, 0.5] * 33
+
+print(max(w)) #max()
+print(min(w)) #min()
